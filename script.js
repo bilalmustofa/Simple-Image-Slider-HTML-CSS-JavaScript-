e06a071acb6401064bbca39c43275e4f721aea6e
bilalmustofa <bilalmustofa146@gmail.com>
2026-04-12 22:32:35 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Next Button
 const rightArrowBtn = document.querySelector(".right-arrow");
 const imageBox = document.querySelector('.image-box');
+const images = document.querySelectorAll('img');
 let currentImage = 1;
 
 rightArrowBtn.addEventListener("click", () => {
+    if (currentImage > images.length) {
+        currentImage = 1;
+    }
     currentImage++;
     imageBox.style.transform = `translateX(-${(currentImage - 1) * 700}px)`;
 });
