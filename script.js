7e29363f89b60f8e7169f140d9f13de00737c757
bilal <bilalmustofa146@gmail.com>
2026-04-16 12:59:53 -0600

    added next and previous function

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // Next Button
-const rightArrowBtn = document.querySelector(".right-arrow");
 const imageBox = document.querySelector('.image-box');
-const images = document.querySelectorAll('img');
-let currentImage = 1;
+const images = document.querySelectorAll(".image-box img");
+let currentImage = 0;
 
-
+// When Click Next Button
+const rightArrowBtn = document.querySelector(".right-arrow");
 rightArrowBtn.addEventListener("click", () => {
-    if(currentImage > images.length){
-        currentImage = 1;
-    }
     currentImage++;
-    imageBox.style.transform = `translateX(-${(currentImage - 1) * 700}px)`;
+    if(currentImage >= images.length){
+        currentImage = 0;
+    }
+    updateSlide();
 }); 
+
+// When Click previous Button 
+const leftArrowBtn = document.querySelector(".left-arrow");
+leftArrowBtn.addEventListener('click', () => {
+    currentImage--;
+    if(currentImage < 0) {
+        currentImage = images.length - 1;
+    }
+    updateSlide();
+})
+
+// update slide 
+function updateSlide() {
+    const width = document.querySelector(".slider-box").clientWidth;
+    console.log(width);
+    imageBox.style.transform = `translateX(-${currentImage * width}px)`;
+}
